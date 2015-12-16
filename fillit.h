cb880220a44d1b4e67aa9c3641eeacd80dc383a3
Pierre GRASSIN <pgrassin@e1r2p16.42.fr>
2015-12-16 16:45:14 +0100

    .h updqte

--- fillit.h
+++ fillit.h
@@ -6,8 +6,19 @@
 /*   By: pgrassin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/15 19:01:38 by pgrassin          #+#    #+#             */
-/*   Updated: 2015/12/15 19:01:40 by pgrassin         ###   ########.fr       */
+/*   Updated: 2015/12/16 16:44:58 by pgrassin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FILLIT_H
+# define FILLIT_H
 
+typedef struct		s_list
+{
+	int		coord1;
+	int		coord2;
+	int		coord3;
+	int		coord4;
+	int		index;
+}					t_list;
+#endif
