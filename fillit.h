bb105d92197b6513f5837ccaf166bb653e27ef4a
Pierre GRASSIN <pgrassin@e1r2p16.42.fr>
2015-12-16 16:49:10 +0100

    .h updqte2

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: pgrassin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/15 19:01:38 by pgrassin          #+#    #+#             */
-/*   Updated: 2015/12/16 16:44:58 by pgrassin         ###   ########.fr       */
+/*   Updated: 2015/12/16 16:48:59 by pgrassin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,11 @@
 
 typedef struct		s_list
 {
-	int		coord1;
-	int		coord2;
-	int		coord3;
-	int		coord4;
-	int		index;
+	int				coord1;
+	int				coord2;
+	int				coord3;
+	int				coord4;
+	int				index;
+	struct s_list	*next;
 }					t_list;
 #endif
