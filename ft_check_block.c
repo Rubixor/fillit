aac2b146075818a5bc0f03d053b9192ee2adeed6
Marie DENOYELLE <mdenoyel@e1r1p13.42.fr>
2015-12-18 16:42:11 +0100

    Update ft_check*

--- ft_check_block.c
+++ ft_check_block.c
@@ -6,7 +6,7 @@
 /*   By: mdenoyel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/16 16:44:37 by mdenoyel          #+#    #+#             */
-/*   Updated: 2015/12/17 17:49:00 by mdenoyel         ###   ########.fr       */
+/*   Updated: 2015/12/18 15:43:07 by mdenoyel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,33 @@
 
 int		ft_addinlist(t_list *list, int x, int y)
 {
-	printf("Entree dans addinlist");
+//	printf("Entree dans addinlist\n");
 	if ((list->coord1) == -1)
+	{
 		list->coord1 = x * 10 + y;
+//		printf("Coordonnee placee en 1: %d\n", list->coord1);
+	}
 	else if ((list->coord2) == -1)
+	{
 		list->coord2 = x * 10 + y;
+//		printf("Coordonnee placee en 2: %d\n", list->coord2);
+	}
 	else if ((list->coord3) == -1)
+	{
 		list->coord3 = x * 10 + y;
+//		printf("Coordonnee placee en 3: %d\n", list->coord3);
+	}
 	else if ((list->coord4) == -1)
+	{
 		list->coord4 = x * 10 + y;
+//		printf("Coordonnee placee en 4: %d\n", list->coord4);
+	}
 	else
+	{
+//		printf("Sortie de addinlist return 0\n");
 		return (0); // ceci est une erreur
-	printf("Sortie de addinlist");
+	}
+//	printf("Sortie de addinlist return 1\n");
 	return (1);
 }
 
@@ -36,57 +51,83 @@ int		ft_check_tetris(int fd, t_list *list, int y)
 	int		x;
 	int		rd;
 
+//	printf("Entree dans ft_check_tetris\n");
 	if ((rd = read(fd, buf, 5) != 0))
 	{
+//		printf("Entree if read != 0 : rd = %d | buf = %s\n", rd, buf);
 		if (rd < 0)
-			return (0);
-		x = 0;
-		while (x < 5)
 		{
+//			printf("rd < 0 : rd = %d\n", rd);
+			return (0);
+		}
+		x = 0;
+		while (x < 4)
+		{
+//			printf("x < 4: x = %d\n", x);
 			if (buf[x] == '#')
 			{
+//				printf("buf[x] = %c\n", buf[x]);
 				if (!(ft_addinlist(list, x, y)))
+				{
+//					printf("Erreur dans addinlist\n");
 					return (0); // error
+				}
 			}
 			else if (buf[x] != '.')
+			{
+//				printf("##buf[x] = %c\n", buf[x]);
 				return (0);
+			}
+			x++;
 		}
 		if (buf[x] != '\n')
+		{
+//			printf("buf[x] != \\n : buf[x] = %c\n", buf[x]);
 			return (0);
+		}
+//		printf("Sortie read, tout est ok\n");
 		return (1);
 	}
+//	printf("rd = 0\n");
 	return (0);
 }
 
-int		ft_check_block(int fd, t_list *list) // si return (0) afficher erreur ailleurs
+int		ft_check_block(int fd, t_list **list) // si return (0) afficher erreur ailleurs
 {
 	int		i;
-	int		y;
 	int		rd;
 	t_list	*new_lst;
 	char	buf[1];
 
-	i = 0;
-	y = 0;
+//	printf("Entree check_block\n");
 	while (1) // attention au segfault
 	{
+		i = 0;
+//		printf("Entree while(1)\n");
 		new_lst = ft_newlst();
-		while (i <= 5)
+		while (i <= 4)
 		{
-			if ((i < 5) && (!(ft_check_tetris(fd, new_lst, y))))
-				return (0);
-			else if (i == 5 && ((rd = read(fd, buf, 1))))
+//			printf("i <= 4: i = %d\n", i);
+			if ((i < 4) && (!(ft_check_tetris(fd, new_lst, i))))
 			{
-				if (rd < 0 || buf[0] != '\n')
-					return (0);
-				else if (rd == 0)
-					return (1); //fin du fichier
+//				printf("ft_check_tetris = 0\n");
+				return (0);
 			}
-			if (ft_check_links(list))
-				ft_lst_pushback(&list, new_lst);
-			ft_lstdel(new_lst);
 			i++;
 		}
-		y++;
+		rd = read(fd, buf, 1);
+//		printf("i = 4, rd = %d\n", rd);
+		if (ft_check_links(new_lst))
+		{
+//			printf("Nombre de liens ok\n");
+			ft_lst_pushback(list, new_lst);
+//			printf("Sortie lst_pushback\n");
+		}
+		else
+			return (0);
+		if (rd == 0)
+			return (1); //fin du fichier
+		else if (rd < 0 || buf[0] != '\n')
+			return (0);
 	}
 }
