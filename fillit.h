d76ce4fecb7a3dd58180ad518fada2c295126882
Marie DENOYELLE <mdenoyel@e1r1p13.42.fr>
2015-12-18 16:41:39 +0100

    .h update 6

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: pgrassin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/15 19:01:38 by pgrassin          #+#    #+#             */
-/*   Updated: 2015/12/17 17:48:57 by mdenoyel         ###   ########.fr       */
+/*   Updated: 2015/12/18 15:35:40 by mdenoyel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ typedef struct		s_list
 
 int					ft_check_links(t_list *list);
 
-int					ft_check_block(int fd, t_list *list);
+int					ft_check_block(int fd, t_list **list);
 
 t_list				*ft_lst_pushback(t_list **lst, t_list *item);
 
