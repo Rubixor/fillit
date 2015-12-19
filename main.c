f62a26714c97de794202f24460dd441864f2d367
Marie DENOYELLE <mdenoyel@e1r2p15.42.fr>
2015-12-19 19:58:29 +0100

    Update main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mdenoyel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/17 17:32:52 by mdenoyel          #+#    #+#             */
-/*   Updated: 2015/12/18 15:46:50 by mdenoyel         ###   ########.fr       */
+/*   Updated: 2015/12/19 19:52:13 by mdenoyel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,14 @@ int		main()
 {
 	int		fd;
 	t_list	*lst;
+	t_map	*m;
 
 	lst = NULL;
 	fd = open("test.txt", O_RDONLY);
 	if (fd)
-		printf("Check block : %d\n", ft_check_block(fd, &lst));
+		ft_check_block(fd, &lst);
+//		printf("Check block : %d\n", ft_check_block(fd, &lst));
+	m = ft_createmap();
 	ft_move_upleft(lst);
 	close(fd);
 	return (0);
