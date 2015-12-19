aa4a3c04bce8fb35cf3830fc81999d25a06b6d1c
Marie DENOYELLE <mdenoyel@e1r2p15.42.fr>
2015-12-19 19:58:11 +0100

    Update move

--- ft_move_upleft.c
+++ ft_move_upleft.c
@@ -6,7 +6,7 @@
 /*   By: mdenoyel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/16 18:48:40 by mdenoyel          #+#    #+#             */
-/*   Updated: 2015/12/18 15:44:31 by mdenoyel         ###   ########.fr       */
+/*   Updated: 2015/12/19 19:21:10 by mdenoyel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,13 @@
 
 void	ft_move_upleft(t_list *list)
 {
-	printf("Entree move\n");
+//	printf("Entree move\n");
+	int		i;
+
+	i = 0;
 	while (list)
 	{
-		printf("while (list)\n");
+//		printf("while (list)\n");
 		while (list->coord1 / 10 != 0 && list->coord2 / 10 != 0
 				&& list->coord3 / 10 != 0 && list->coord4 / 10 != 0)
 		{
@@ -34,8 +37,11 @@ void	ft_move_upleft(t_list *list)
 			list->coord3 -= 1;
 			list->coord4 -= 1;
 		}
-		printf("coord1 = %d, coord2 = %d, coord3 = %d, coord4 = %d\n",
-				list->coord1, list->coord2, list->coord3, list->coord4);
+		list->index = i;
+		i++;
+//		printf("coord1 = %d, coord2 = %d, coord3 = %d, coord4 = %d, index = %d\n"
+//				, list->coord1, list->coord2, list->coord3,
+//				list->coord4, list->index);
 		list = list->next;
 	}
 }
